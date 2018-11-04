1ba2d4dc757868281a7d80350e6624c4238b2264
MoralesKevin <moraleskevin1502@gmail.com>
2018-11-04 18:08:37 +0100

    Updates

---
+++ precache-manifest.40cbe115f866884281930693272dd06c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d33d5874e5442390dd1965c18ec214a",
+    "url": "/Calculatrice/static/media/Digital-7.5d33d587.ttf"
+  },
+  {
+    "revision": "961eb85b0f94e12abe86",
+    "url": "/Calculatrice/static/js/runtime~main.961eb85b.js"
+  },
+  {
+    "revision": "8229cdb79fba5fd4ae0e",
+    "url": "/Calculatrice/static/js/main.8229cdb7.chunk.js"
+  },
+  {
+    "revision": "c19b3b67c5defa888732",
+    "url": "/Calculatrice/static/js/1.c19b3b67.chunk.js"
+  },
+  {
+    "revision": "8229cdb79fba5fd4ae0e",
+    "url": "/Calculatrice/static/css/main.11ebec48.chunk.css"
+  },
+  {
+    "revision": "da94b92fc112bac0ae16b8a0b66567ac",
+    "url": "/Calculatrice/index.html"
+  }
+];
--- src/reducers/Operateurs.jsx
+++
@@ -1,8 +0,0 @@
-const Operateurs = [
-    { id: 11, valeur: "/" },
-    { id: 12, valeur: "*" },
-    { id: 13, valeur: "-" },
-    { id: 14, valeur: "+" },
-]
-
-export default Operateurs;
